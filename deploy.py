30be24f105fcc9b855881533c776a22c55a2cf80
Shuaib Ahmed <shuaib.ahmed45@gmail.com>
2025-11-29 23:19:33 +0000

    Switch to flow.deploy() with Prefect managed work pool

--- deploy.py
+++ deploy.py
@@ -1,8 +1,9 @@
 from email_flow import example_email_send_message_flow
 
 if __name__ == "__main__":
-    example_email_send_message_flow.serve(
+    example_email_send_message_flow.deploy(
         name="email-deployment",
+        work_pool_name="prefect-managed",
         tags=["email", "automation"],
         description="Sends scheduled emails to carers with payment information",
     )
